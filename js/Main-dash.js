4ea45e9d368157a92cb597dfa4d3c1434509d691
TheDIABLO20 <64171730+TheDIABLO20@users.noreply.github.com>
2026-07-19 08:31:19 -0500

    Update Main-dash.js

--- js/Main-dash.js
+++ js/Main-dash.js
@@ -1,14 +1,12 @@
+console.log("APP CARGADA");
 const supabaseUrl =
-    "https://drxvkseiacmrzedbzysc.supabase.co";
-
+"https://drxvkseiacmrzedbzysc.supabase.co";
 const supabaseKey =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRyeHZrc2VpYWNtcnplZGJ6eXNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0Mzk3NjIsImV4cCI6MjEwMDAxNTc2Mn0.j3KDqYF6Q-hED46CQlDvMa4yByOrqkT0nnFy3glrwBE";
-
-const supabase =
-    window.supabase.createClient(
-        supabaseUrl,
-        supabaseKey
-    );
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRyeHZrc2VpYWNtcnplZGJ6eXNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0Mzk3NjIsImV4cCI6MjEwMDAxNTc2Mn0.j3KDqYF6Q-hED46CQlDvMa4yByOrqkT0nnFy3glrwBE";
+const supabaseClient = window.supabase.createClient(
+    supabaseUrl,
+    supabaseKey
+);
 
 function esAdmin(){
 
