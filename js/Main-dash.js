a57b1420e4cd0cf86f1c0d2c79d9f8362559f490
TheDIABLO20 <64171730+TheDIABLO20@users.noreply.github.com>
2026-07-19 08:18:24 -0500

    Update Main-dash.js

--- js/Main-dash.js
+++ js/Main-dash.js
@@ -1,3 +1,15 @@
+const supabaseUrl =
+    "https://drxvkseiacmrzedbzysc.supabase.co";
+
+const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRyeHZrc2VpYWNtcnplZGJ6eXNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0Mzk3NjIsImV4cCI6MjEwMDAxNTc2Mn0.j3KDqYF6Q-hED46CQlDvMa4yByOrqkT0nnFy3glrwBE";
+
+const supabase =
+    window.supabase.createClient(
+        supabaseUrl,
+        supabaseKey
+    );
+
 function esAdmin(){
 
     return localStorage.getItem("esAdmin")
@@ -74,7 +86,7 @@ async function guardarPPM(){
     };
 
     const { error } =
-        await supabase
+        await 
             .from("ppm")
             .insert([ppm]);
 
@@ -169,7 +181,7 @@ function cargarPPM(){
 async function cargarPPM(){
 
     const { data, error } =
-        await supabase
+        await 
             .from("ppm")
             .select("*")
             .order("id", {
