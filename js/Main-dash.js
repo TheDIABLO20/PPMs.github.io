29461ddfafd3c989f4617423ce9897e461e6400b
TheDIABLO20 <64171730+TheDIABLO20@users.noreply.github.com>
2026-08-16 14:34:28 -0500

    Update Main-dash.js

--- js/Main-dash.js
+++ js/Main-dash.js
@@ -640,6 +640,28 @@ document.getElementById("qrqc_hallazgo").textContent = "";
 
 }
 
+function mostrarQR(){
+
+    const ppmId =
+        document.getElementById("empleado").value || "nuevo";
+
+    const urlSubida =
+        `Z:\BrayanS\Pruebas\PPMS?id=${ppmId}`;
+
+    const qr =
+        `https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=${encodeURIComponent(urlSubida)}`;
+
+    document.getElementById("contenedorQR").innerHTML = `
+        <h3>Escanea este QR</h3>
+
+        ${qr}
+
+        <p>
+            Usa tu teléfono para subir evidencias.
+        </p>
+    `;
+}
+
 function cargarUsuario(){
 
     const nombre =
